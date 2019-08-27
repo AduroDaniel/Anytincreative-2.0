32b9b78adb7e831625bcc23b09ef6eac87163c38
Daniel Aduro <42278732+AduroDaniel@users.noreply.github.com>
2019-08-27 19:17:51 +0100

    Commit registerstyle.scss

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Question Array
+const questions = [
+    { question: 'Enter Your First Name' },
+    { question: 'Enter Your Last Name' },
+    { question: 'Enter Your Email', pattern: /\S+@\S+\.\S+/ },
+    { question: 'Enter Your Phone' }
+];
+
+//Transition Times
+const shakeTime = 100;
